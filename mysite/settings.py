58e2a3225f1f2958a378df466888b2a2306af2dd
SJTU-Boring <Boring123@sjtu.edu.cn>
2024-11-08 18:16:41 +0800

    modified:   .gitignore 	modified:   mysite/settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':  'shopping_management',
         'USER': 'root',
-        'PASSWORD': 'hjmmysql2003',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': 3306
      }
